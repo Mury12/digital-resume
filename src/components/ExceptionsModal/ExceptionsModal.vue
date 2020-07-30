bbadbf6bd87246e5482e5f0d5170616959d9ca2f
Andre Mury <andre.mury@primeinterway.com.br>
2020-07-30 19:14:19 -0300

    feat: paginação nas exceções

--- src/components/ExceptionsModal/ExceptionsModal.vue
+++ src/components/ExceptionsModal/ExceptionsModal.vue
@@ -2,32 +2,59 @@
   <b-modal :id="name" size="xl" title="Exceções de Negócio" ok-only ok-title="Voltar">
     <b-tabs>
       <b-tab title="Aéreo" v-if="exceptionList && exceptionList.air.length">
+        <b-pagination
+          v-model="curPage.air"
+          :total-rows="exceptionList.air.length"
+          :per-page="perPage"
+          aria-controls="airtable"
+        ></b-pagination>
         <b-table
+          id="airtable"
           ref="airtable"
           :fields="fields"
           :items="exceptionList.air"
           sticky-header
           bordered
+          :per-page="perPage"
+          :current-page="curPage.air"
           style="min-height: 40vh"
         ></b-table>
       </b-tab>
       <b-tab title="Marítimo" v-if="exceptionList && exceptionList.maritime.length">
+        <b-pagination
+          v-model="curPage.lcl"
+          :total-rows="exceptionList.maritime.length"
+          :per-page="perPage"
+          aria-controls="lcltable"
+        ></b-pagination>
         <b-table
+          id="lcltable"
           ref="lcltable"
           :fields="fields"
           :items="exceptionList.maritime"
           sticky-header
           bordered
+          :per-page="perPage"
+          :current-page="curPage.lcl"
           style="min-height: 40vh"
         ></b-table>
       </b-tab>
       <b-tab title="Banco de Dados" v-if="exceptionList && exceptionList.database.length">
+        <b-pagination
+          v-model="curPage.database"
+          :total-rows="exceptionList.database.length"
+          :per-page="perPage"
+          aria-controls="airtable"
+        ></b-pagination>
         <b-table
+          id="dbtable"
           ref="dbtable"
           :fields="fields"
           :items="exceptionList.database"
           sticky-header
           bordered
+          :per-page="perPage"
+          :current-page="curPage.database"
           style="min-height: 40vh"
         ></b-table>
       </b-tab>
@@ -45,6 +72,12 @@ export default {
         air: [],
         database: [],
       },
+      curPage: {
+        air: 0,
+        lcl: 0,
+        database: 0,
+      },
+      perPage: 100,
       fields: [
         {
           key: "itemKey",
@@ -61,8 +94,8 @@ export default {
     exceptions: Array,
     name: {
       type: String,
-      default: 'exceptions_modal'
-    }
+      default: "exceptions_modal",
+    },
   },
   watch: {
     exceptions() {
